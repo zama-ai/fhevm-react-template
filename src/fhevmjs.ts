dbf548eafa7ac308e7af24788d96f647bec54976
Clément 'birdy' Danjou <clement@danjou.io>
2024-01-18 13:27:52 +0100

    deps() update to fhevmjs 0.3.1

--- src/fhevmjs.ts
+++ src/fhevmjs.ts
@@ -1,5 +1,7 @@
-import { BrowserProvider } from 'ethers';
+import { BrowserProvider, AbiCoder } from 'ethers';
 import { initFhevm, createInstance, FhevmInstance } from 'fhevmjs';
+
+const FHE_LIB_ADDRESS = '0x000000000000000000000000000000000000005d';
 
 export const init = async () => {
   await initFhevm();
@@ -11,21 +13,24 @@ export const createFhevmInstance = async () => {
   const provider = new BrowserProvider(window.ethereum);
   const network = await provider.getNetwork();
   const chainId = +network.chainId.toString();
-  const publicKey = await provider.call({
-    from: null,
-    to: '0x0000000000000000000000000000000000000044',
+  const ret = await provider.call({
+    to: FHE_LIB_ADDRESS,
+    // first four bytes of keccak256('fhePubKey(bytes1)') + 1 byte for library
+    data: '0xd9d47bb001',
   });
+  const decoded = AbiCoder.defaultAbiCoder().decode(['bytes'], ret);
+  const publicKey = decoded[0];
   instance = await createInstance({ chainId, publicKey });
 };
 
-export const getTokenSignature = async (contractAddress: string, userAddress: string) => {
+export const getSignature = async (contractAddress: string, userAddress: string) => {
   if (getInstance().hasKeypair(contractAddress)) {
-    return getInstance().getTokenSignature(contractAddress)!;
+    return getInstance().getPublicKey(contractAddress)!;
   } else {
-    const { publicKey, token } = getInstance().generateToken({ verifyingContract: contractAddress });
-    const params = [userAddress, JSON.stringify(token)];
+    const { publicKey, eip712 } = getInstance().generatePublicKey({ verifyingContract: contractAddress });
+    const params = [userAddress, JSON.stringify(eip712)];
     const signature: string = await window.ethereum.request({ method: 'eth_signTypedData_v4', params });
-    getInstance().setTokenSignature(contractAddress, signature);
+    getInstance().setSignature(contractAddress, signature);
     return { signature, publicKey };
   }
 };
