7667166bf1c9640dc31bd7d1519d5a125cf1ecac
Aylen Reynoso <areynoso@fi.uba.ar>
2024-12-14 00:54:57 -0300

    all test pass added has voted mapping

--- hardhat/contracts/ballot.sol
+++ hardhat/contracts/ballot.sol
@@ -13,51 +13,55 @@ contract Ballot is
     SepoliaZamaFHEVMConfig,
     SepoliaZamaGatewayConfig,
     GatewayCaller{
-
+    
     struct Proposal {
         string name;
-        uint256 index;
+        //uint256 index;
         uint256 voteCount;
     }
     mapping(uint256 => Proposal) public proposals;
+    mapping(address => bool) public hasVoted;
 
-
-    euint4 unoEncriptado;
-    euint4 ceroEncriptado;
+    euint4 eOne;
+    euint4 eZero;
 
     uint256 public startTime;
     uint256 public duration;
     uint256 public proposalCount;
     bool public ballotFinished;
 
-    constructor(uint256 duration) {
-        unoEncriptado = TFHE.asEuint4(1);
-        ceroEncriptado = TFHE.asEuint4(0);
+    constructor(uint256 _duration) {
+        eOne = TFHE.asEuint4(1);
+        eZero = TFHE.asEuint4(0);
         startTime = 0;
-        duration = duration;
+        duration = _duration;
         proposalCount = 0;
         ballotFinished = false;
     }
 
     function createProposal(string memory proposalName) public {
-        proposals[proposalCount] = Proposal({name: proposalName, index: proposalCount, voteCount: 0});
+        require(startTime == 0, "Ballot has already started - cannot add new proposals");
+        proposals[proposalCount] = Proposal({name: proposalName, voteCount: 0});
         proposalCount++;
     }
 
-    function getProposal(uint256 index) public view returns (Proposal memory) {
-        return proposals[index];
+    function getProposal(uint256 _index) public view returns (Proposal memory) {
+        return proposals[_index];
     }
 
     function startBallot() public { 
         startTime = block.timestamp;
     }
 
-    function vote(uint256 proposal) public {
+    function vote(uint256 _proposal) public {
+        require(!ballotFinished, "Ballot is finished");
+        require(!hasVoted[msg.sender], "Already voted");
+        
         for (uint256 i = 0; i < proposalCount; i++) {
-            if (i == proposal) {
-                proposals[i].voteCount += 1; //unoEncriptado
+            if (i == _proposal) {
+                proposals[i].voteCount += 1; //eOne
             } else {
-                proposals[i].voteCount += 0; //ceroEncriptado
+                proposals[i].voteCount += 0; //eZero
             }
         }
     }
--- hardhat/test/ballot/Ballot.fixture.ts
+++ hardhat/test/ballot/Ballot.fixture.ts
@@ -4,11 +4,11 @@ import type { Ballot } from "../../types";
 import { getSigners } from "../signers";
 
 export async function deployBallot(): Promise<Ballot> {
-    const signers = await getSigners();
+  const signers = await getSigners();
 
-    const contractFactory = await ethers.getContractFactory("Ballot");
-    const contract = await contractFactory.connect(signers.alice).deploy("1000"); // City of Zama's battle
-    await contract.waitForDeployment();
+  const contractFactory = await ethers.getContractFactory("Ballot");
+  const contract = await contractFactory.connect(signers.alice).deploy("1"); // City of Zama's battle
+  await contract.waitForDeployment();
 
-    return contract;
+  return contract;
 }
