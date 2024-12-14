f1086365f2ca1d4815c07ee96ee9b60ef4647436
Manu <frosimanuel@gmail.com>
2024-12-13 21:48:09 -0300

    ballot con tests andando todos menos el ultimo que por alguna razon no revierte, por que sera? cuentenme en sus comentarios cual es su hipotesis

--- hardhat/contracts/ballot.sol
+++ hardhat/contracts/ballot.sol
@@ -12,8 +12,7 @@ import "fhevm-contracts/contracts/token/ERC20/extensions/ConfidentialERC20Mintab
 contract Ballot is
     SepoliaZamaFHEVMConfig,
     SepoliaZamaGatewayConfig,
-    GatewayCaller,
-    ConfidentialERC20Mintable{
+    GatewayCaller{
 
     struct Proposal {
         string name;
@@ -22,12 +21,18 @@ contract Ballot is
     }
     mapping(uint256 => Proposal) public proposals;
 
+
+    euint4 unoEncriptado;
+    euint4 ceroEncriptado;
+
     uint256 public startTime;
     uint256 public duration;
     uint256 public proposalCount;
     bool public ballotFinished;
 
     constructor(uint256 duration) {
+        unoEncriptado = TFHE.asEuint4(1);
+        ceroEncriptado = TFHE.asEuint4(0);
         startTime = 0;
         duration = duration;
         proposalCount = 0;
@@ -47,18 +52,23 @@ contract Ballot is
         startTime = block.timestamp;
     }
 
-    function vote(uint256 proposal, bool voter) public {
+    function vote(uint256 proposal) public {
         for (uint256 i = 0; i < proposalCount; i++) {
-            if (i != proposal) {
-                proposals[i].voteCount += 0;
+            if (i == proposal) {
+                proposals[i].voteCount += 1; //unoEncriptado
+            } else {
+                proposals[i].voteCount += 0; //ceroEncriptado
             }
         }
-        //proposals[proposal].voteCount += voter ? 1 : -1;
     }
 
     function finishBallot() public {
         require(block.timestamp >= startTime + duration, "Ballot is still ongoing");
         ballotFinished = true;
+    }
+
+    function get_ballot_status() public view returns (bool) {
+        return ballotFinished;
     }
 
     function get_result() public view returns (Proposal memory) {
