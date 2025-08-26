068bdfe7987793fb28359073e828dacf8cc4f195
“ObitoStack” <thuytien.huflit@gmail.com>
2025-08-26 23:47:42 +0700

    feature: create useZmailManager

--- packages/site/components/MailApp.tsx
+++ packages/site/components/MailApp.tsx
@@ -1,144 +1,80 @@
 import "@/styles/mail-app.css";
 
-import { useState, useRef, useEffect } from "react";
-
 import { Toaster } from "react-hot-toast";
 import LoadingBar from "react-top-loading-bar";
 
 import MailEditor from "./mail/MailEditor";
 import MailSidebar from "./mail/MailSidebar";
-import MailHeader from "./mail/MailHeader/index";
+import MailHeader from "./mail/MailHeader";
 import MailItemList from "./mail/MailItemList";
 import MailThread from "./mail/MailThread";
 
-import { useFHEZmail } from "@/hooks/useFHEZmail";
-import { useInMemoryStorage } from "@/hooks/useInMemoryStorage";
-
-import { Mail, LoadingBarRef } from "@/types";
-import { TAB_INDEXES, TabIndex } from "@/constants/index";
+import { useZmailManager } from "@/hooks/useZmailManager";
 
 export default function MailApp() {
-  const { storage: fhevmDecryptionSignatureStorage } = useInMemoryStorage();
-  const { isInitialized, sendMail } = useFHEZmail({
-    fhevmDecryptionSignatureStorage,
-  });
-
-  const [isOpenEditor, setIsOpenEditor] = useState<boolean>(false);
-  const [activeTab, setActiveTab] = useState<TabIndex>(TAB_INDEXES.INBOX);
-  const [activeTabCount, setActiveTabCount] = useState<number>(0);
-
-  const [searchValue, setSearchValue] = useState<string>("");
-  const [isSearching, setIsSearching] = useState<boolean>(false);
-  const [filteredMails, setFilteredMails] = useState<Mail[]>([]);
-
-  const [isSelecting, setIsSelecting] = useState<boolean>(false);
-  const [activeMailId, setActiveMailId] = useState<number | null>(null);
-  const [selectedMailIds, setSelectedMailIds] = useState<number[]>([]);
-
-  const [isReplying, setIsReplying] = useState<boolean>(false);
-  const [isForwarding, setIsForwarding] = useState<boolean>(false);
-
-  const [bulkActionType, setBulkActionType] = useState<string>("");
-  const loadingBarRef = useRef<LoadingBarRef["current"]>(null);
-
-  const [threadMails, setThreadMails] = useState<Mail[]>([]);
-
-  const [mails, setMails] = useState<Mail[]>([]);
-  const refreshMap: Record<number, () => void> = {
-    [TAB_INDEXES.STARRED]: () => console.log("refresh starred"),
-    [TAB_INDEXES.SENT]: () => console.log("refresh sent"),
-    [TAB_INDEXES.ARCHIVE]: () => console.log("refresh archive"),
-    [TAB_INDEXES.SPAM]: () => console.log("refresh spam"),
-    [TAB_INDEXES.READ]: () => console.log("refresh read"),
-    [TAB_INDEXES.TRASH]: () => console.log("refresh trash"),
-  };
-  const refreshByTab = (tabIndex: number): (() => void) => {
-    return refreshMap[tabIndex] ?? (() => {});
-  };
-
-  const getMailIds = (): number[] => {
-    return mails.map((mail) => mail.id);
-  };
-
-  const executeBulkAction = async () => {};
-
-  const onReply = async () => {};
-
-  const onForward = async () => {};
-
-  useEffect(() => {
-    if (isInitialized) {}
-  }, [isInitialized]);
-
-  useEffect(() => {
-    setIsSelecting(false);
-    setActiveMailId(null);
-    setIsReplying(false);
-    setIsForwarding(false);
-    setSelectedMailIds([]);
-  }, [activeTab]);
+  const { state, setters, refs, methods } = useZmailManager();
 
   return (
     <div className="mailApp">
       <MailEditor
-        isOpenEditor={isOpenEditor}
-        setIsOpenEditor={setIsOpenEditor}
-        sendMail={sendMail}
+        isOpenEditor={state.isOpenEditor}
+        setIsOpenEditor={setters.setIsOpenEditor}
+        sendMail={methods.sendMail}
       />
 
       <div className="mailBody">
         <MailSidebar
-          activeTab={activeTab}
-          activeTabCount={activeTabCount}
-          setActiveTab={setActiveTab}
-          setIsOpenEditor={setIsOpenEditor}
+          activeTab={state.activeTab}
+          activeTabCount={state.activeTabCount}
+          setActiveTab={setters.setActiveTab}
+          setIsOpenEditor={setters.setIsOpenEditor}
         />
 
         <div className="mailContent">
           <MailHeader
-            activeTab={activeTab}
-            searchValue={searchValue}
-            setSearchValue={setSearchValue}
-            isSelecting={isSelecting}
-            setIsSelecting={setIsSelecting}
-            activeMailId={activeMailId}
-            mailIds={getMailIds()}
-            setActiveMailId={setActiveMailId}
-            selectedMailIds={selectedMailIds}
-            setSelectedMailIds={setSelectedMailIds}
-            isReplying={isReplying}
-            setIsReplying={setIsReplying}
-            isForwarding={isForwarding}
-            setIsForwarding={setIsForwarding}
-            bulkActionType={bulkActionType}
-            setBulkActionType={setBulkActionType}
-            executeBulkAction={executeBulkAction}
-            loadingBarRef={loadingBarRef}
+            activeTab={state.activeTab}
+            searchValue={state.searchValue}
+            setSearchValue={setters.setSearchValue}
+            isSelecting={state.isSelecting}
+            setIsSelecting={setters.setIsSelecting}
+            activeMailId={state.activeMailId}
+            mailIds={methods.getMailIds()}
+            setActiveMailId={setters.setActiveMailId}
+            selectedMailIds={state.selectedMailIds}
+            setSelectedMailIds={setters.setSelectedMailIds}
+            isReplying={state.isReplying}
+            setIsReplying={setters.setIsReplying}
+            isForwarding={state.isForwarding}
+            setIsForwarding={setters.setIsForwarding}
+            bulkActionType={state.bulkActionType}
+            setBulkActionType={setters.setBulkActionType}
+            executeBulkAction={methods.executeBulkAction}
+            loadingBarRef={refs.loadingBarRef}
           />
-          {activeMailId === null ? (
+          {state.activeMailId === null ? (
             <MailItemList
-              mails={mails}
-              filteredMails={filteredMails}
-              activeTab={activeTab}
-              isSearching={isSearching}
-              setIsSelecting={setIsSelecting}
-              selectedMailIds={selectedMailIds}
-              setActiveMailId={setActiveMailId}
-              setSelectedMailIds={setSelectedMailIds}
+              mails={state.mails}
+              filteredMails={state.filteredMails}
+              activeTab={state.activeTab}
+              isSearching={state.isSearching}
+              setIsSelecting={setters.setIsSelecting}
+              selectedMailIds={state.selectedMailIds}
+              setActiveMailId={setters.setActiveMailId}
+              setSelectedMailIds={setters.setSelectedMailIds}
             />
           ) : (
             <MailThread
-              activeMail={activeMailId !== null ? mails[activeMailId] : null}
-              threadMails={threadMails}
-              setThreadMails={setThreadMails}
-              onReply={onReply}
-              onForward={onForward}
+              activeMail={state.activeMailId !== null ? state.mails[state.activeMailId] : null}
+              threadMails={state.threadMails}
+              setThreadMails={setters.setThreadMails}
+              onReply={methods.onReply}
+              onForward={methods.onForward}
             />
           )}
         </div>
       </div>
 
-      <LoadingBar color="#FED218" ref={loadingBarRef} />
+      <LoadingBar color="#FED218" ref={refs.loadingBarRef} />
       <Toaster position="bottom-center" toastOptions={{ className: "toast" }} />
     </div>
   );
