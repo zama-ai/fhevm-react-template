51b4811cdda9e9c1fc308383d441448099b6c795
“ObitoStack” <thuytien.huflit@gmail.com>
2025-08-30 14:16:55 +0700

    feature: update contract abi and address

--- packages/site/abi/FHEZmailAddresses.ts
+++ packages/site/abi/FHEZmailAddresses.ts
@@ -1,6 +1,6 @@
 export const FHEZmailAddresses: Record<string, { address: string; chainId: number; chainName: string }> = {
   "11155111": {
-    address: "0xDa58297c887cae4956c609B94f598502450A1dDD",
+    address: "0xe5197d70457B22a4c79358F7149849B03b2249b8",
     chainId: 11155111,
     chainName: "sepolia",
   },
