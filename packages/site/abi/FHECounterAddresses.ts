80c68b0753740c4c8e5b9a727215f306ad404cb2
0xalexbel <alexandre.belhoste@zama.ai>
2025-09-10 17:44:40 +0200

    feat: fhevm v0.8.0 + polish error messages

--- packages/site/abi/FHECounterAddresses.ts
+++
@@ -1,9 +0,0 @@
-
-/*
-  This file is auto-generated.
-  Command: 'npm run genabi'
-*/
-export const FHECounterAddresses = { 
-  "11155111": { address: "0x0000000000000000000000000000000000000000", chainId: 11155111, chainName: "sepolia" },
-  "31337": { address: "0x7553CB9124f974Ee475E5cE45482F90d5B6076BC", chainId: 31337, chainName: "hardhat" },
-};
--- packages/site/components/ErrorNotDeployed.tsx
+++ packages/site/components/ErrorNotDeployed.tsx
@@ -11,18 +11,24 @@ export function errorNotDeployed(chainId: number | undefined) {
           {chainId === 11155111 ? "(Sepolia)" : ""} or Deployment Address
           Missing.
         </p>
-        <p className="text-2xl leading-relaxed mt-8">
-          It appears that the FHECounter.sol contract has either not been
-          deployed yet, or the deployment address is missing from the ABI
-          directory{" "}
-          <span className="font-mono bg-white">root/packages/abi</span>. Run the
-          following command:
+        <p className="text-xl leading-relaxed mt-8">
+          It appears that the{" "}
+          <span className="font-mono bg-white">FHECounter.sol</span> contract
+          has either not been deployed yet, or the deployment address is missing
+          from the ABI directory{" "}
+          <span className="font-mono bg-white">root/packages/site/abi</span>. To
+          deploy <span className="font-mono bg-white">FHECounter.sol</span> on
+          Sepolia, run the following command:
         </p>
         <p className="font-mono text-2xl leading-relaxed bg-black text-white p-4 mt-12">
+          <span className="opacity-50 italic text-red-500">
+            #from &lt;root&gt;/packages/fhevm-hardhat-template
+          </span>
+          <br />
           npx hardhat deploy --network{" "}
           {chainId === 11155111 ? "sepolia" : "your-network-name"}
         </p>
-        <p className="text-2xl leading-relaxed mt-12">
+        <p className="text-xl leading-relaxed mt-12">
           Alternatively, switch to the local{" "}
           <span className="font-mono bg-white">Hardhat Node</span> using the
           MetaMask browser extension.
